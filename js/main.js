93d7602fd884a9a28a294d22b1f30434968404b4
moriyamasaaaki <moriya-7071@outlook.com>
2019-08-15 22:45:11 +0900

    shuffle

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@
   });
 
   function shuffle(arr) {
-    for (let i = arr.length - 1; i < 0; i--) {
+    for (let i = arr.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [arr[j], arr[i]] = [arr[i], arr[j]];
     }
